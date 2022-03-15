f6a1c767ea58ab7ce5a85dff0c25b0d134365683
namanchhikara <namanchhikara@gmail.com>
2022-03-14 20:00:06 -0400

    clean

--- app.js
+++ app.js
@@ -9,31 +9,12 @@ app.set('view engine', 'ejs');
 app.get("/", function(req, res){
     var today = new Date();
     var day;
-    switch (today.getDay()) {
-        case 0:
-            day = 'Sunday';
-            break;
-        case 1:
-            day = 'Monday';
-            break;
-        case 2:
-            day = 'Tuesday';
-            break;
-        case 3:
-            day = 'Wednesday';
-            break;
-        case 4:
-            day = 'Thursday';
-            break;
-        case 5:
-            day = 'Friday';
-            break;
-        case 6:
-            day = 'Saturday';
-            break;
-        default:
-            break;
-    }
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", options);
     res.render("list", {
         kindOfDay: day
     });
